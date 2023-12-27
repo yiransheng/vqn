5d70123df9016d0c46e6a6bb3646aeaa96f8b145
Yiran Sheng <shengyiran@gmail.com>
2023-12-27 10:00:43 +0000

    prettify shell script

--- vqn-cli/src/firewall.rs
+++ vqn-cli/src/firewall.rs
@@ -1,4 +1,6 @@
-use std::process::Command;
+use std::net::IpAddr;
+
+use cmd_lib::run_cmd;
 
 use crate::conf::{Cidr, Conf, Network};
 
@@ -11,115 +13,48 @@ pub fn dev_up(conf: &Conf) {
         Network::Server { client, .. } => {
             for c in client {
                 for ip in &c.allowed_ips.values {
-                    route_allowed_ip("add", tun, *ip, routing_table);
+                    route_allowed_ip(tun, *ip, routing_table);
                 }
             }
         }
         Network::Client { server, .. } => {
             for ip in &server.allowed_ips.values {
-                route_allowed_ip("add", tun, *ip, routing_table);
+                route_allowed_ip(tun, *ip, routing_table);
             }
         }
     }
-
-    for ipv in ["-4", "-6"] {
-        cmd(
-            "ip",
-            [
-                ipv,
-                "rule",
-                "add",
-                "not",
-                "fwmark",
-                &fwmark,
-                "table",
-                routing_table,
-            ]
-            .as_slice(),
-        );
-        cmd(
-            "ip",
-            [
-                ipv,
-                "rule",
-                "add",
-                "table",
-                "main",
-                "suppress_prefixlength",
-                "0",
-            ]
-            .as_slice(),
-        );
-    }
-    cmd("resolvectl", ["dns", tun, "1.1.1.1"].as_slice());
+    let _ = run_cmd! {
+        ip -4 rule add not fwmark $fwmark table $routing_table;
+        ip -4 rule add table main suppress_prefixlength 0;
+        ip -6 rule add not fwmark $fwmark table $routing_table;
+        ip -6 rule add table main suppress_prefixlength 0;
+        resolvectl dns tun0 1.1.1.1;
+    };
 }
 
 pub fn dev_down(conf: &Conf) {
     let routing_table = "19988";
     let fwmark = conf.network.fwmark().unwrap_or(19988).to_string();
 
-    for ipv in ["-4", "-6"] {
-        cmd(
-            "ip",
-            [
-                ipv,
-                "rule",
-                "delete",
-                "not",
-                "fwmark",
-                &fwmark,
-                "table",
-                routing_table,
-            ]
-            .as_slice(),
-        );
-        cmd(
-            "ip",
-            [
-                ipv,
-                "rule",
-                "delete",
-                "table",
-                "main",
-                "suppress_prefixlength",
-                "0",
-            ]
-            .as_slice(),
-        );
-    }
-    cmd("ip", ["rule", "delete", "table", routing_table].as_slice());
-}
+    let _ = run_cmd! {
+        ip -4 rule delete not fwmark $fwmark table $routing_table;
+        ip -4 rule delete table main suppress_prefixlength 0;
 
-fn route_allowed_ip(action: &str, tun: &str, ip: Cidr, table: &str) {
-    let ipv = match ip.ip() {
-        std::net::IpAddr::V4(_) => "-4",
-        std::net::IpAddr::V6(_) => "-6",
+        ip -6 rule delete not fwmark $fwmark table $routing_table;
+        ip -6 rule delete table main suppress_prefixlength 0;
+
+        ip rule delete table $routing_table;
     };
-    cmd(
-        "ip",
-        [
-            ipv,
-            "route",
-            action,
-            &ip.to_string(),
-            "dev",
-            tun,
-            "table",
-            table,
-        ]
-        .as_slice(),
-    )
 }
 
-fn cmd(cmd: &str, args: &[&str]) {
-    let exit_code = Command::new(cmd)
-        .args(args)
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
-
-    if !exit_code.success() {
-        tracing::error!("Failed to execte {}, {:?}", cmd, args);
-    }
+fn route_allowed_ip(tun: &str, ip: Cidr, table: &str) {
+    let ip_ = ip.to_string();
+    let _ = match ip.ip() {
+        IpAddr::V4(_) => run_cmd! {
+            ip -4 route add $ip_ dev $tun table $table;
+        },
+        IpAddr::V6(_) => run_cmd! {
+            ip -6 route add $ip_ dev $tun table $table;
+        },
+    };
 }
