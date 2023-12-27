37af0a698edf6a40acaf05cffc6d8ba6f4d8c2c5
Yiran Sheng <shengyiran@gmail.com>
2023-12-27 22:23:04 +0000

    add setup scripts

--- README.md
+++ README.md
@@ -16,3 +16,33 @@ pub async fn run(tun: Tun, conn: Connection) {
     }
 }
 ```
+
+## Perf
+
+Direct:
+```
+Connecting to host www.vqn.org, port 10086
+[  5] local 192.168.10.6 port 57380 connected to 192.0.2.1 port 10086
+[ ID] Interval           Transfer     Bitrate         Retr  Cwnd
+[  5]   0.00-20.00  sec  30.0 MBytes  12.6 Mbits/sec   11    588 KBytes       
+- - - - - - - - - - - - - - - - - - - - - - - - -
+[ ID] Interval           Transfer     Bitrate         Retr
+[  5]   0.00-20.00  sec  30.0 MBytes  12.6 Mbits/sec   11             sender
+[  5]   0.00-20.31  sec  27.3 MBytes  11.3 Mbits/sec                  receiver
+
+iperf Done.
+```
+
+vqn:
+```
+Connecting to host 10.10.0.1, port 1024
+[  5] local 10.10.0.3 port 53802 connected to 10.10.0.1 port 1024
+[ ID] Interval           Transfer     Bitrate         Retr  Cwnd
+[  5]   0.00-20.00  sec  36.8 MBytes  15.4 Mbits/sec   39    607 KBytes       
+- - - - - - - - - - - - - - - - - - - - - - - - -
+[ ID] Interval           Transfer     Bitrate         Retr
+[  5]   0.00-20.00  sec  36.8 MBytes  15.4 Mbits/sec   39             sender
+[  5]   0.00-20.30  sec  36.1 MBytes  14.9 Mbits/sec                  receiver
+
+iperf Done.
+```
---
+++ set_me_up/steps.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+function step_0() {
+    echo "Step 0: Generate openssl.cnf"
+    echo "Type a domain name (it does not need to be a real one):"
+    read -r domain_name
+
+    if [[ -f "openssl.cnf.example" ]]; then
+        sed "s/<mydomain.com>/${domain_name}/g" openssl.cnf.example > openssl.cnf
+        echo "openssl.cnf has been cnfigured with the domain: $domain_name"
+        echo $domain_name > step_0.out
+    else
+        echo "Error: openssl.cnf.example does not exist."
+    fi
+}
+
+function step_1() {
+    domain_name=`cat step_0.out`
+    echo "Edit your /etc/hosts file to map your server ip to $domain_name"
+    echo "PRESS ENTER to confirm you have done so"
+
+    read -r 
+    echo "done" > step_1.out
+}
+
+function step_2() {
+    echo "Configure client.toml and server.toml from the provided examples in this directory."
+    echo "The required certs (.pem) files are make targets."
+    echo "(examine Makefile for openssl commands used)"
+    echo "PRESS ENTER to confirm you have done so"
+
+    read -r 
+    echo "done" > step_2.out
+}
+
+printf "\n\n"
+
+case "$1" in
+    step_0)
+        step_0
+        ;;
+    step_1)
+        step_1
+        ;;
+    step_2)
+        step_2
+        ;;
+    step_3)
+        step_3
+        ;;
+    *)
+        echo "No matching function found for the argument: $1"
+        ;;
+esac
